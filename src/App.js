78878155c3f057ce68ed7bc1c8a45b0586e87878
nk-on <johndoe@example.com>
2024-11-01 22:11:34 +0400

    Puttin componets inside App.js deleting components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
-import Card from './components/Card/Card';
 function App() {
   return <><Card /></>;
+}
+function Card() {
+  return <div className="Card">
+    <ContentPassive/>
+  </div>;
+}
+function ContentPassive() {
+  return (
+    <>
+      <div className="content">
+        <h1>How did we do?</h1>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+        <div className="buttons">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </div>
+        <button className="Submit-button">Submit</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Card/Card.js
+++
@@ -1,7 +0,0 @@
-import './Card.css';
-import ContentPassive from '../Content-passive/ContentPassive';
-export default function Card() {
-  return <div className="Card">
-    <ContentPassive/>
-  </div>;
-}
